763b6cba674a87f6d49955d517a508379ac1c264
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-10 23:58:17 +0200

    Add Day partial and further settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+// const { fontFamily } = require('tailwindcss/defaultTheme')
+
+// font-family: 'Bebas Neue', cursive;
+// font-family: 'Radio Canada', sans-serif;
 module.exports = {
+  darkMode: 'class',
   content: [
-    './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Bebas Neue','Radio Canada', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  }
 }
